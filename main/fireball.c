27ced4dd851c56766e29dd0d4698eeb5476f09d0
ClaudioCGO <144236323+ClaudioCGO@users.noreply.github.com>
2026-06-12 00:01:58 -0300

    Callibration changes

--- main/fireball.c
+++ main/fireball.c
@@ -10,7 +10,7 @@
 #include "tcrt5000.h"
 
 #define THRESHOLD 2000
-#define FINISH_TIME 35
+#define FINISH_TIME 100
 
 #define MAX_SPEED 3500 
 #define MED_SPEED 2000 
@@ -35,17 +35,21 @@ void app_main(void)
 
     bool finished = false;
 
+    printf("WAITING 3 SECONDS...");
+    vTaskDelay(pdMS_TO_TICKS(3000));
+
     while (!finished)
     {
         ir_read(&le, &li, &ri, &re);
 
-        bool b_le = le < THRESHOLD;
-        bool b_li = li < THRESHOLD;
-        bool b_ri = ri < THRESHOLD;
-        bool b_re = re < THRESHOLD;
+        bool b_le = le > THRESHOLD;
+        bool b_li = li > 900;
+        bool b_ri = ri > 600;
+        bool b_re = re > THRESHOLD;
 
         if (b_le && b_li && b_ri && b_re) {
             finish_counter++;
+            printf("DO NOTHING... %d    ", finish_counter);
             if (finish_counter >= FINISH_TIME) {
                 motors_brake();
                 set_PWM(0, 0);
@@ -61,46 +65,53 @@ void app_main(void)
                 motors_yaw_left();
                 set_PWM(TURN_SPEED, TURN_SPEED);
                 last_error = -2;
+                printf("HARD TURN LEFT   ");
             }
 
             else if (b_re) {
                 motors_yaw_right();
                 set_PWM(TURN_SPEED, TURN_SPEED);
                 last_error = 2;
+                printf("HARD TURN RIGHT   ");
             }
 
             else if (b_li && !b_ri) {
                 motors_forward();
                 set_PWM(MED_SPEED, MAX_SPEED);
                 last_error = -1;
+                printf("SMALL TURN LEFT   ");
             }
 
             else if (!b_li && b_ri) {
                 motors_forward();
                 set_PWM(MAX_SPEED, TURN_SPEED);
                 last_error = 1;
+                printf("SMALL TURN RIGHT   ");
             }
 
-            else if (b_li && b_re) {
+            else if (b_li && b_ri) {
                 motors_forward();
                 set_PWM(MAX_SPEED, MAX_SPEED);
-                last_error = 0;
+                printf("STRAIGHT   ");
             }
 
             else {
                 if (last_error < 0) {
                     motors_yaw_left();
                     set_PWM(TURN_SPEED, TURN_SPEED);
+                    printf("SEARCHING LEFT   ");
                 }
 
                 else if (last_error > 0) {
                     motors_yaw_right();
-                    set_PWM(TURN_SPEED, TURN_SPEED);     
+                    set_PWM(TURN_SPEED, TURN_SPEED);
+                    printf("SEARCHING RIGHT   ");     
                 }
 
                 else {
                     motors_brake();
                     set_PWM(0, 0);
+                    printf("NOTHING TO SEARCH   ");
                 }
             }
         }

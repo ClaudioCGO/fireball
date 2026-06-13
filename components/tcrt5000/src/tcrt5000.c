94cc348e3d0d4870e7194bc4443c83aff0312d40
João H. G. Pereira <156421880+jhgpereira@users.noreply.github.com>
2026-06-13 18:11:04 -0300

    Move sensors log to drive_task on fireball.c

--- components/tcrt5000/src/tcrt5000.c
+++ components/tcrt5000/src/tcrt5000.c
@@ -71,6 +71,6 @@ void ir_read(int *le, int *li, int *ri, int *re)
   adc_oneshot_read(adc1_handle, ADC_CHANNEL_1, li);
   adc_oneshot_read(adc1_handle, ADC_CHANNEL_3, re);
   adc_oneshot_read(adc1_handle, ADC_CHANNEL_4, ri);
-  printf("LE: %d | LI: %d | RI: %d | RE: %d\n", *le, *li, *ri, *re);
-  fflush(stdout);
+//  printf("LE: %d | LI: %d | RI: %d | RE: %d\n", *le, *li, *ri, *re);
+//  fflush(stdout);
 }

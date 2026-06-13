a2f50bee59390c8e9b35423b2827529b8c99d46f
João H. G. Pereira <156421880+jhgpereira@users.noreply.github.com>
2026-06-13 18:19:35 -0300

    Changes on pwm speeds, log on esp_logi instead of printf, formating code blocks, treashhoulds for each sensor, sensors log on drive_task, stop condition count updated, work frequency updated for more cicles

--- main/fireball.c
+++ main/fireball.c
@@ -8,13 +8,12 @@
 #include "pwm.h"
 #include "tcrt5000.h"
 
-#define THRESHOLD 2000
 #define FINISH_TIME 100
+#define MAX_SPEED 1200
+#define MED_SPEED 1000
+#define TURN_SPEED 1200
 
-#define MAX_SPEED 2000
-#define MED_SPEED 1800
-#define TURN_SPEED 2000
-
+static const char *TAG = "fireball";
 static int le, li, ri, re;
 static int last_error = 0;
 static int finish_counter = 0;
@@ -23,89 +22,98 @@ static volatile bool finished = false;
 void drive_task(void *pvParameters)
 {
   while (!finished)
+  {
+    ir_read(&le, &li, &ri, &re);
+
+    bool b_le = le > 2200; // W = 1200; B = 3150; T = 2200
+    bool b_li = li > 1220; // W = 560; B = 1880; T = 1220
+    bool b_ri = ri > 800; // W = 360; B = 1250; T = 800
+    bool b_re = re > 2100; // W = 1200; B = 3100; T = 2100
+
+//  bool b_le = le > 900;
+//  bool b_li = li > 720;
+//  bool b_ri = ri > 400;
+//  bool b_re = re > 1480;
+
+    if (b_le && b_li && b_ri && b_re)
     {
-      ir_read(&le, &li, &ri, &re);
-
-      bool b_le = le > 900;
-      bool b_li = li > 720;
-      bool b_ri = ri > 400;
-      bool b_re = re > 1480;
-
-      if (b_le && b_li && b_ri && b_re)
+      finish_counter++;
+      ESP_LOGI(TAG, "DO NOTHING... %d", finish_counter);
+      if (finish_counter >= FINISH_TIME)
       {
-        finish_counter++;
-        printf("DO NOTHING... %d    ", finish_counter);
-        if (finish_counter >= FINISH_TIME)
-        {
-          motors_brake();
-          set_PWM(0, 0);
-          finished = true;
-          printf("Ended!");
-        }
+        motors_brake();
+        set_PWM(0, 0);
+        finished = true;
+        ESP_LOGI(TAG, "Ended!");
+      }
+    }
+    else
+    {
+      finish_counter = 0;
+
+      if (b_le)
+      {
+        motors_yaw_left();
+        set_PWM(TURN_SPEED, TURN_SPEED);
+        last_error = -2;
+        ESP_LOGI(TAG, "HARD TURN LEFT");
+      }
+      else
+      if (b_re)
+      {
+        motors_yaw_right();
+        set_PWM(TURN_SPEED, TURN_SPEED);
+        last_error = 2;
+        ESP_LOGI(TAG, "HARD TURN RIGHT");
+      }
+      else
+      if (b_li && !b_ri)
+      {
+        motors_forward();
+        set_PWM(MED_SPEED, MAX_SPEED);
+        last_error = -1;
+        ESP_LOGI(TAG, "SMALL TURN LEFT");
+      }
+      else
+      if (!b_li && b_ri)
+      {
+        motors_forward();
+        set_PWM(MAX_SPEED, MED_SPEED);
+        last_error = 1;
+        ESP_LOGI(TAG, "SMALL TURN RIGHT");
+      }
+      else
+      if (b_li && b_ri)
+      {
+        motors_forward();
+        set_PWM(MAX_SPEED, MAX_SPEED);
+        ESP_LOGI(TAG, "STRAIGHT");
+      }
+      else
+      if (last_error < 0)
+      {
+        motors_yaw_left();
+        set_PWM(TURN_SPEED, TURN_SPEED);
+        ESP_LOGI(TAG, "SEARCHING LEFT");
+      }
+      else
+      if (last_error > 0)
+      {
+        motors_yaw_right();
+        set_PWM(TURN_SPEED, TURN_SPEED);
+        ESP_LOGI(TAG, "SEARCHING RIGHT");
       }
       else
       {
-        finish_counter = 0;
-
-        if (b_le) {
-          motors_yaw_left();
-          set_PWM(TURN_SPEED, TURN_SPEED);
-          last_error = -2;
-          printf("HARD TURN LEFT   ");
-        }
-        else if (b_re)
-        {
-          motors_yaw_right();
-          set_PWM(TURN_SPEED, TURN_SPEED);
-          last_error = 2;
-          printf("HARD TURN RIGHT   ");
-        } else if (b_li && !b_ri) {
-                motors_forward();
-                set_PWM(MED_SPEED, MAX_SPEED);
-                last_error = -1;
-                printf("SMALL TURN LEFT   ");
-            }
-
-            else if (!b_li && b_ri) {
-                motors_forward();
-                set_PWM(MAX_SPEED, MED_SPEED);
-                last_error = 1;
-                printf("SMALL TURN RIGHT   ");
-            }
-
-            else if (b_li && b_ri) {
-                motors_forward();
-                set_PWM(MAX_SPEED, MAX_SPEED);
-                printf("STRAIGHT   ");
-            }
-
-            else {
-                if (last_error < 0) {
-                    motors_yaw_left();
-                    set_PWM(TURN_SPEED, TURN_SPEED);
-                    printf("SEARCHING LEFT   ");
-                }
-
-                else if (last_error > 0) {
-                    motors_yaw_right();
-                    set_PWM(TURN_SPEED, TURN_SPEED);
-                    printf("SEARCHING RIGHT   ");     
-                }
-
-                else {
-                    motors_brake();
-                    set_PWM(0, 0);
-                    printf("NOTHING TO SEARCH   ");
-                }
-            }
-        }
-      
-      vTaskDelay(pdMS_TO_TICKS(100));
-
+        motors_brake();
+        set_PWM(0, 0);
+        ESP_LOGI(TAG, "NOTHING TO SEARCH");
       }
-    
-    vTaskDelete(NULL);
-
+    }
+    ESP_LOGI(TAG,"LE: %d | LI: %d | RI: %d | RE: %d", le, li, ri, re);
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
+  vTaskDelete(NULL);
 }
 
 void app_main(void)
@@ -120,7 +128,7 @@ void app_main(void)
 
     init_adc_channels();
 
-    printf("WAITING 3 SECONDS...");
+    ESP_LOGI(TAG, "WAITING 3 SECONDS...");
     vTaskDelay(pdMS_TO_TICKS(3000));
 
     xTaskCreate(

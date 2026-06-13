895ba094e21bdb5bfdb93a35b83fb35af745c2ab
João H. G. Pereira <pereirajoao3775@gmail.com>
2026-06-13 11:58:05 -0300

    Refactor fireball test controller, and first tests on FreeRTOS Task version of controller

--- main/fireball.c
+++ main/fireball.c
@@ -3,7 +3,6 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
-
 #include "board.h"
 #include "motors.h"
 #include "pwm.h"
@@ -12,70 +11,55 @@
 #define THRESHOLD 2000
 #define FINISH_TIME 100
 
-#define MAX_SPEED 3500 
-#define MED_SPEED 2000 
-#define TURN_SPEED 2500 
+#define MAX_SPEED 2000
+#define MED_SPEED 1800
+#define TURN_SPEED 2000
 
+static int le, li, ri, re;
+static int last_error = 0;
+static int finish_counter = 0;
+static volatile bool finished = false;
 
-void app_main(void)
+void drive_task(void *pvParameters)
 {
-    motors_control_init();
-
-    pwm_config_t pwm_cfg = {
-        .left_pin = HB_PWMA,
-        .right_pin = HB_PWMB
-    };
-    configure_pwm(pwm_cfg);
-
-    init_adc_channels();
-
-    int le, li, ri, re;
-    int last_error = 0; 
-    int finish_counter = 0;
-
-    bool finished = false;
-
-    printf("WAITING 3 SECONDS...");
-    vTaskDelay(pdMS_TO_TICKS(3000));
-
-    while (!finished)
+  while (!finished)
     {
-        ir_read(&le, &li, &ri, &re);
+      ir_read(&le, &li, &ri, &re);
 
-        bool b_le = le > THRESHOLD;
-        bool b_li = li > 900;
-        bool b_ri = ri > 600;
-        bool b_re = re > THRESHOLD;
+      bool b_le = le > 900;
+      bool b_li = li > 720;
+      bool b_ri = ri > 400;
+      bool b_re = re > 1480;
 
-        if (b_le && b_li && b_ri && b_re) {
-            finish_counter++;
-            printf("DO NOTHING... %d    ", finish_counter);
-            if (finish_counter >= FINISH_TIME) {
-                motors_brake();
-                set_PWM(0, 0);
-                finished = true;
-                printf("Ended!");
-            }
+      if (b_le && b_li && b_ri && b_re)
+      {
+        finish_counter++;
+        printf("DO NOTHING... %d    ", finish_counter);
+        if (finish_counter >= FINISH_TIME)
+        {
+          motors_brake();
+          set_PWM(0, 0);
+          finished = true;
+          printf("Ended!");
         }
+      }
+      else
+      {
+        finish_counter = 0;
 
-        else {
-            finish_counter = 0;
-
-            if (b_le) {
-                motors_yaw_left();
-                set_PWM(TURN_SPEED, TURN_SPEED);
-                last_error = -2;
-                printf("HARD TURN LEFT   ");
-            }
-
-            else if (b_re) {
-                motors_yaw_right();
-                set_PWM(TURN_SPEED, TURN_SPEED);
-                last_error = 2;
-                printf("HARD TURN RIGHT   ");
-            }
-
-            else if (b_li && !b_ri) {
+        if (b_le) {
+          motors_yaw_left();
+          set_PWM(TURN_SPEED, TURN_SPEED);
+          last_error = -2;
+          printf("HARD TURN LEFT   ");
+        }
+        else if (b_re)
+        {
+          motors_yaw_right();
+          set_PWM(TURN_SPEED, TURN_SPEED);
+          last_error = 2;
+          printf("HARD TURN RIGHT   ");
+        } else if (b_li && !b_ri) {
                 motors_forward();
                 set_PWM(MED_SPEED, MAX_SPEED);
                 last_error = -1;
@@ -84,7 +68,7 @@ void app_main(void)
 
             else if (!b_li && b_ri) {
                 motors_forward();
-                set_PWM(MAX_SPEED, TURN_SPEED);
+                set_PWM(MAX_SPEED, MED_SPEED);
                 last_error = 1;
                 printf("SMALL TURN RIGHT   ");
             }
@@ -115,6 +99,36 @@ void app_main(void)
                 }
             }
         }
-        vTaskDelay(pdMS_TO_TICKS(50));
-    }
+      
+      vTaskDelay(pdMS_TO_TICKS(100));
+
+      }
+    
+    vTaskDelete(NULL);
+
+}
+
+void app_main(void)
+{
+    motors_control_init();
+
+    pwm_config_t pwm_cfg = {
+        .left_pin = HB_PWMA,
+        .right_pin = HB_PWMB
+    };
+    configure_pwm(pwm_cfg);
+
+    init_adc_channels();
+
+    printf("WAITING 3 SECONDS...");
+    vTaskDelay(pdMS_TO_TICKS(3000));
+
+    xTaskCreate(
+        drive_task,
+        "drive_task",
+        4096,
+        NULL,
+        5,
+        NULL
+    );
 }

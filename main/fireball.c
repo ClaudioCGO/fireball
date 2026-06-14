8d5b8d1fe5fae38b3f9dd90e6e88f696800de5f4
ClaudioCGO <144236323+ClaudioCGO@users.noreply.github.com>
2026-06-13 21:22:23 -0300

    Divided drive_task in two different tasks

--- main/fireball.c
+++ main/fireball.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "esp_log.h"
 #include "board.h"
 #include "motors.h"
@@ -9,6 +10,7 @@
 #include "tcrt5000.h"
 
 #define FINISH_TIME 100
+
 #define MAX_SPEED 1200
 #define MED_SPEED 1000
 #define TURN_SPEED 1200
@@ -18,24 +20,49 @@
 #define THRESHOLD_RI 800  // W = 360; B = 1250; T = 800
 #define THRESHOLD_RE 2100 // W = 1200; B = 3100; T = 2100
 
+typedef struct {
+    bool b_le;
+    bool b_li;
+    bool b_ri;
+    bool b_re;
+} sensor_data_t;
+
+QueueHandle_t sensor_queue;
+
 static const char *TAG = "fireball";
 
-static int le, li, ri, re;
-static int last_error = 0;
-static int finish_counter = 0;
 static volatile bool finished = false;
 
-void drive_task(void *pvParameters) {
+void sensor_task (void *pvParameters) {
+    int le, li, ri, re;
+    sensor_data_t current_reading;
+
     while (!finished) {
         ir_read(&le, &li, &ri, &re);
 
-        bool b_le = le > THRESHOLD_LE;
-        bool b_li = li > THRESHOLD_LI;
-        bool b_ri = ri > THRESHOLD_RI;
-        bool b_re = re > THRESHOLD_RE;
+        current_reading.b_le = le > THRESHOLD_LE;
+        current_reading.b_li = li > THRESHOLD_LI;
+        current_reading.b_ri = ri > THRESHOLD_RI;
+        current_reading.b_re = re > THRESHOLD_RE;
 
+        xQueueOverwrite(sensor_queue, &current_reading);
 
-        if (b_le && b_li && b_ri && b_re) {
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+    vTaskDelete(NULL);
+}
+
+void control_task (void *pvParameters) {
+    sensor_data_t data;
+    int last_error = 0;
+    int finish_counter = 0;
+   
+    while (!finished) {
+        if (xQueueReceive(sensor_queue, &data, pdMS_TO_TICKS(100)) != pdPASS) {
+            continue;
+        }        
+
+        if (data.b_le && data.b_li && data.b_ri && data.b_re) {
             finish_counter++;
 
             ESP_LOGI(TAG, "DO NOTHING AND ACCELERATE | Count: %d", finish_counter);
@@ -53,7 +80,7 @@ void drive_task(void *pvParameters) {
 
         finish_counter = 0;
 
-        if (b_le) {
+        if (data.b_le) {
             ESP_LOGI(TAG, "HARD TURN LEFT");
             motors_yaw_left();
             set_PWM(TURN_SPEED, TURN_SPEED);
@@ -61,7 +88,7 @@ void drive_task(void *pvParameters) {
             continue;
         }
 
-        if (b_re) {
+        if (data.b_re) {
             ESP_LOGI(TAG, "HARD TURN RIGHT");
             motors_yaw_right();
             set_PWM(TURN_SPEED, TURN_SPEED);
@@ -69,7 +96,7 @@ void drive_task(void *pvParameters) {
             continue;
         }
 
-        if (b_li && !b_ri) {
+        if (data.b_li && !data.b_ri) {
             ESP_LOGI(TAG, "SMALL TURN LEFT");
             motors_forward();
             set_PWM(MED_SPEED, MAX_SPEED);
@@ -77,7 +104,7 @@ void drive_task(void *pvParameters) {
             continue;
         }
 
-        if (!b_li && b_ri) {
+        if (!data.b_li && data.b_ri) {
             ESP_LOGI(TAG, "SMALL TURN RIGHT");
             motors_forward();
             set_PWM(MAX_SPEED, MED_SPEED);
@@ -85,13 +112,12 @@ void drive_task(void *pvParameters) {
             continue;
         }
 
-        if (b_li && b_ri) {
+        if (data.b_li && data.b_ri) {
             ESP_LOGI(TAG, "STRAIGHT");
             motors_forward();
             set_PWM(MAX_SPEED, MAX_SPEED);
             continue;
         }
-
 
         if (last_error < 0) {
             ESP_LOGI(TAG, "SEARCHING LEFT");
@@ -111,15 +137,11 @@ void drive_task(void *pvParameters) {
             motors_brake();
             set_PWM(0, 0);
         }
-    
-        ESP_LOGI(TAG,"LE: %d | LI: %d | RI: %d | RE: %d", le, li, ri, re);
-        vTaskDelay(pdMS_TO_TICKS(10));
     }
   vTaskDelete(NULL);
 }
 
-void app_main(void)
-{
+void app_main (void) {
     motors_control_init();
 
     pwm_config_t pwm_cfg = {
@@ -133,5 +155,14 @@ void app_main(void)
     ESP_LOGI(TAG, "WAITING 3 SECONDS...");
     vTaskDelay(pdMS_TO_TICKS(3000));
 
-    xTaskCreate(drive_task, "drive_task", 4096, NULL, 5, NULL);
+    sensor_queue = xQueueCreate(1, sizeof(sensor_data_t));
+
+    if (sensor_queue = NULL) {
+        ESP_LOGI(TAG, "FAILED TO CREATE QUEUE!");
+    }
+
+    else {
+        xTaskCreate(sensor_task, "sensor_task", 4096, NULL, 4, NULL);
+        xTaskCreate(control_task, "control_task", 4096, NULL, 5, NULL);
+    }
 }
